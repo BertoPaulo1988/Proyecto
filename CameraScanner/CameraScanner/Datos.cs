be3300d531e0a0b38aed7b29186639da73d4ec14
Alberto <berto__@outlook.es>
2019-04-13 20:10:48 +0200

    Clase Principal y Datos
    Se agrega la clase principal y la secundaria (aun en pruebas).
    La aplicacion da error la primera vez que se instala , luego funciona correctamente.

---
+++ CameraScanner/CameraScanner/Datos.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using Android.Support.Design.Widget;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Xamarin.Forms;
+using Android.Content.PM;
+
+
+
+namespace CameraScanner
+{
+    //Applicaion siempre en vertical
+    [Activity(Label = "Archivos Guardados", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class Datos : AppCompatActivity
+    {
+        //Declaracion de variables
+
+
+        public Android.Widget.ListView lista;
+        //Array para el nombre de los ficheros de la carpeta
+        private List<String> nombres = new List<string>();
+        //Ruta donde buscar los ficheros
+        String ruta;
+        //Direcctorio de los ficheros
+        File directorio;
+        //Array de tipo File con el contenido de la carpeta
+        File[] archivos;
+        ArrayAdapter<string> listaArchivos;
+        private File archivo;
+
+
+        //Pruebas para abrir archivo
+        String paaath;
+        String noooom;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Inicializacion de variables
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Datos);
+
+            lista = FindViewById<Android.Widget.ListView>(Resource.Id.lista);
+            nombres = new List<string>();
+            ruta = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/CameraScanner";
+            directorio = new File(ruta);
+            archivos = directorio.ListFiles();
+
+            //Boton volver
+            Android.Widget.Button volver = FindViewById<Android.Widget.Button>(Resource.Id.btn_volver);
+            //Evento del boton volver
+            volver.Click += Volver;
+
+            //Boton flotante para borrado (No funcional/En pruebas)
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += Borrado;
+
+            CrearListado();
+
+
+
+            //Prueba de evento en la lista            
+            lista.ItemClick += AbrirArchivo;
+
+            lista.ItemLongClick += Marcar;
+        }
+
+
+        //Metodo que busca archivos en el directorio y los agrega a la lista
+        private void CrearListado()
+        {
+            //Comprobamos que tengamos archivos en el directorio
+            if (archivos.Length != 0)
+            {
+                //Agregamos el nombre de los archivos a la lista de nombres
+                for (int i = 0; i < archivos.Length; i++)
+                {
+                    archivo = archivos[i];
+                    nombres.Add(archivo.Name);
+                }
+            }
+            //Localizamos y llenamos la lista con el array
+            listaArchivos = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, nombres);
+            lista.Adapter = listaArchivos;
+        }
+
+
+        //Metodo para abrir archivos al pulsar en ellos en la lista (No funcional/En pruebas)
+        private void AbrirArchivo(object sender, EventArgs eventArgs)
+        {
+
+            Android.Views.View view = (Android.Views.View)sender;
+            // Snackbar.Make(view, "Opcion no válida sin permisos de lectura/escritura", Snackbar.LengthLong).SetAction("Action", (View.IOnClickListener)null).Show();
+            // Xamarin.Forms.Device.OpenUri(new Uri(archivos[i].Name));
+            for (int i = 0; i < archivos.Length; i++)
+            {
+
+
+            }
+
+        }
+        //Metodo para abrir archivos al pulsar en ellos en la lista (No funcional/En pruebas)       
+        public void AbrirArchivo2(string ruta, string tipo)
+        {
+            var uri = Android.Net.Uri.Parse("file://" + ruta);
+            var intent = new Intent(Intent.ActionView);
+            intent.SetDataAndType(uri, tipo);
+            intent.SetFlags(ActivityFlags.ClearWhenTaskReset | ActivityFlags.NewTask);
+
+            try
+            {
+                this.StartActivity(Intent.CreateChooser(intent, "Seleccione app"));
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        public void Marcar(object sender, EventArgs eventArgs)
+        {
+
+
+
+        }
+
+        //Metodo para volver al activity anterior.
+        private void Volver(object sender, EventArgs eventArgs)
+        {
+            this.Finish();
+        }
+
+        //Metodo para borrar archivos de la lista (No funcional/En pruebas)  .
+        private void Borrado(object sender, EventArgs eventArgs)
+        {
+
+        }
+    }
+}
--- CameraScanner/CameraScanner/MainActivity.cs
+++ CameraScanner/CameraScanner/MainActivity.cs
@@ -1,19 +1,278 @@
-﻿using Android.App;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Graphics;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
 using Android.Widget;
+using static Android.Gms.Vision.Detector;
 
+
+//IMPORTANTE:
+//Hay que importar en el android SDK manager "Google play services" (En la opcion extras). Luego, en Referencias del proyecto
+//hacemos clic derecho y agregamos "Xamarin.GooglePlayServices.Vision (V-60.1142.1)","Xamarin.Android.Support.v7.AppCompact (V-27.0.2)",
+//"Xamarin.Android.Support.v4 (V-27.0.2)" y  "Xamarin.Forms (V-3.6.0.293080)" en paquetes NuGet.
+//Es necesario que las versiones de "Xamarin.Android.Support.v7.AppCompact" y "Xamarin.Android.Support.v4"
+//sean la (V-27.0.2) para no dar problemas con Xamarin.Forms.
 namespace CameraScanner
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    //Applicaion siempre en vertical
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : AppCompatActivity, ISurfaceHolderCallback, IProcessor
+    //ISurfaceHolderCallback es utilizada para detectar cambios en la Surface. 
+    //IProcessor: Interfaz necesaria para el funcionamiento de la api de Google.
     {
+
+        //Declaracion de variables.
+        private CameraSource cameraSource;
+        private const int RequestCameraPermissionID = 1001;
+        private SurfaceView cameraView;
+        //TextRecognizer encuentra texto dentro de un frame.
+        private TextRecognizer textRecognizer;
+        public TextView texto;
+        private Button botonCaptura, botonDatos;
+        private bool capturarTexto = false;
+
+        //Variable con la ruta para el archivo.
+        public string ruta;
+        //Segundo Activity
+        Intent intent;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
+
+            //Establecer nuestra vista "main"
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //SurfaceView
+            cameraView = FindViewById<SurfaceView>(Resource.Id.surface_view);
+
+            //TextView
+            texto = FindViewById<TextView>(Resource.Id.text_view);
+
+            //Botones
+            botonCaptura = FindViewById<Button>(Resource.Id.btn_CapturaTexto);
+            botonDatos = FindViewById<Button>(Resource.Id.btn_datos);
+            //Evento clic boton datos
+            botonDatos.Click += DatosGuardados;
+
+            //Obtener permisos lectura/escritura
+            PermisosLecturaEscritura();
+
+            //Crear directorio de la app
+            CreacionDirectorioApp();
+
+            //TextRecognizer
+            textRecognizer = new TextRecognizer.Builder(ApplicationContext).Build();
+            if (!textRecognizer.IsOperational)
+                Log.Error("Aviso:", "Detector no disponible!");
+            else
+            {
+                // Caracteristicas del CameraSource
+                cameraSource = new CameraSource.Builder(ApplicationContext, textRecognizer)
+                    .SetFacing(CameraFacing.Back) //Camara seleccionada
+                    .SetRequestedPreviewSize(1920, 1080) //Tamaño de la vista previa en uso por la camara
+                    .SetRequestedFps(2.0f) //Velocidad de fotogramas (En fotogramas por segundo)
+                    .SetAutoFocusEnabled(true) //Autoenfoque
+                    .Build();
+                cameraView.Holder.AddCallback(this);//Recibe informacion del SurfaceView.                
+                textRecognizer.SetProcessor(this);//Estable Iprocessor para el textRecognizer.               
+            }
+
+            //Al producirse el evento Click del boton llama al metodo Capturar.
+            botonCaptura.Click += Capturar;
+        }
+
+
+        //Metodo sobreescritura de los permisos de la camara.
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            switch (requestCode)
+            {
+                case RequestCameraPermissionID:
+
+                    if (grantResults[0] == Permission.Granted)
+                    {
+                        cameraSource.Start(cameraView.Holder);
+                        CreacionDirectorioApp();
+                    }
+                    break;
+            }
+        }
+
+
+        //Metodo que activa la captura de texto.
+        private void Capturar(object sender, EventArgs eventArgs)
+        {
+            //Comprueba si existe el directorio
+            if (CreacionDirectorioApp())
+            {
+                capturarTexto = true;
+            }
+            else
+            {
+                View view = (View)sender;
+                Snackbar.Make(view, "Opcion no válida sin permisos de lectura/escritura", Snackbar.LengthLong).SetAction("Action", (View.IOnClickListener)null).Show();
+            }
+        }
+
+        //Metodo de ISurfaceHolderCallback
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+
+        }
+        //Metodo de ISurfaceHolderCallback
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            //Comprueba permisos de acceso a la camara.
+            if (ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.Camera) == Permission.Granted)
+            {
+                //Si hay permisos de camara se inicia cameraSource
+                cameraSource.Start(cameraView.Holder);
+            }
+
+        }
+        //Metodo de ISurfaceHolderCallback
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+            //Parar cameraSource al cerrar la app o cambiar de activity.
+            cameraSource.Stop();
+        }
+
+        //(Metodo de IProcessor) Metodo que recibe de la API de Google el texto detectado.
+        public void ReceiveDetections(Detections detections)
+        {
+            SparseArray items = detections.DetectedItems;
+            List<TextBlock> listaTexto = new List<TextBlock>();
+            StringBuilder cadenaTexto = new StringBuilder();
+            //Funcion Lambda
+            texto.Post(() =>
+            {
+                for (int cont = 0; cont < items.Size(); cont++)
+                {
+                    //Agrega los "bloques de texto" a listaTexto
+                    listaTexto.Add((TextBlock)items.ValueAt(cont));
+                }
+                foreach (TextBlock item in listaTexto)
+                {
+                    //Adjunta TextBlock on a StringBuilder
+                    cadenaTexto.Append(item.Value);
+                    cadenaTexto.Append("\n");
+                }
+                //Actualiza el TextView
+                texto.Text = cadenaTexto.ToString();
+                // Creacion del archivo de texto en la memoria del dispositivo
+                if (capturarTexto)
+                {
+                    //Creacion del nombre del archivo guardado (En este caso se usa la fecha actual como nombre).                   
+                    String nombreArchivo = DateTime.Now.ToString();
+                    //Quitar caracteres invalidos para los nombres de archivo.
+                    nombreArchivo = nombreArchivo.Replace(':', '-');
+                    nombreArchivo = nombreArchivo.Replace('/', '-');
+                    //Creación de la ruta completa con la ruta mas el nombre del archivo.
+                    string rutaCompleta = System.IO.Path.Combine(ruta, nombreArchivo.ToString() + ".txt");
+                    GeneradorArchivos(cadenaTexto, rutaCompleta);
+                    Toast.MakeText(this.ApplicationContext, "Texto Capturado!", ToastLength.Short).Show();
+                }
+            });
+        }
+
+        //Metodo para guardar los archivos en la memoria del dispositivo.
+        public void GeneradorArchivos(StringBuilder caracteres, String archivo)
+        {
+            //Comprueba que existe el directorio.
+            if (CreacionDirectorioApp())
+            {
+                using (var streamWriter = new StreamWriter(archivo, true))
+                {
+                    streamWriter.WriteLine(caracteres);
+                }
+                capturarTexto = false;
+            }
+        }
+
+        //Metodo de IProcessor.
+        public void Release() { }
+
+        //Metodo para obtener permisos lectura/escritura en memoria del telf.
+        private void PermisosLecturaEscritura()
+        {
+            if ((int)Build.VERSION.SdkInt < 21)
+            {
+                return;
+            }
+            else
+            {
+                //Comprueba permisos de acceso a la camara, lectura y escritura.
+                if (ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.Camera) != Permission.Granted
+                    || ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.WriteExternalStorage) != Permission.Granted
+                    || ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.ReadExternalStorage) != Permission.Granted)
+                {
+                    //Solicitar permisos para usar la API de Google.
+                    ActivityCompat.RequestPermissions(this, new string[]
+                    {
+                     //Escribir los permisos en el manifest
+                   Manifest.Permission.Camera,Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage
+                    }, RequestCameraPermissionID);
+                    return;
+                }
+            }
+        }
+
+        //Metodo para crear el directorio de archivos guardados de la app.
+        private bool CreacionDirectorioApp()
+        {
+            //Variable con la ruta de la app.
+            ruta = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/CameraScanner";
+            //Comprueba permisos lectura/escritura
+            if (PackageManager.CheckPermission(Manifest.Permission.ReadExternalStorage, PackageName).Equals(Permission.Granted)
+                    && PackageManager.CheckPermission(Manifest.Permission.WriteExternalStorage, PackageName).Equals(Permission.Granted))
+            {
+                //Creacion del direcctorio si no existe.
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        //Metodo para abrir segunda Activity si hay directorio de archivos creado.
+        private void DatosGuardados(object sender, EventArgs eventArgs)
+        {
+            //Snackbar.Make(view, "Opcion no activada por ahora", Snackbar.LengthLong).SetAction("Action", (View.IOnClickListener)null).Show();
+
+            //Iniciar activity Datos si hay directorio creado
+            if (CreacionDirectorioApp())
+            {
+                //Segundo Activity
+                intent = new Intent(this, typeof(Datos));
+                this.StartActivity(intent);
+            }
+            else
+            {
+                View view = (View)sender;
+                Snackbar.Make(view, "Opcion no válida sin permisos de lectura/escritura", Snackbar.LengthLong).SetAction("Action", (View.IOnClickListener)null).Show();
+            }
+
         }
     }
+
 }
